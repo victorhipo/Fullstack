d2ea7e38beac63edbb2e630cdd14d0ca83f20de6
Víctor Villajos <victorvillajos@gmail.com>
2026-04-27 17:10:52 +0200

    feat: Añadir clase Result para manejo de resultados y errores

---
+++ TechNotes.domain/Abstractions/Result.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace TechNotes.domain.Abstractions;
+
+public class Result
+{
+    public bool IsSuccessful { get; set; }
+    public bool HasFailed => !IsSuccessful;
+    public string? ErrorMessage { get; }
+
+    public Result(bool isSuccessful, string? errorMessage = null)
+    {
+        IsSuccessful = isSuccessful;
+        ErrorMessage = errorMessage;
+    }
+
+    public static Result Ok() => new(true);
+    public static Result Fail(string errorMessage) => new(false, errorMessage);
+    public static Result<T> Ok<T>(T? value)=> new(value, true, string.Empty);
+    public static Result<T> Fail<T>(string errorMessage)=> new(default, false, errorMessage);
+
+}
+
+public class Result<T>: Result
+{
+    public  T? Value { get; }
+    protected internal Result(T?value, bool isSuccessful, string? errorMessage): base(isSuccessful, errorMessage)
+    {
+        Value = value;
+    }
+}
